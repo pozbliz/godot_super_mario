ffd404d9c7b12b7777439a5d3dd353c57e493544
pozbliz <sebro@hushmail.com>
2025-08-15 23:11:04 +0900

    fix: fix duck state to disallow going to run state directly

--- player/states/ducking_state.gd
+++ player/states/ducking_state.gd
@@ -7,11 +7,10 @@ func enter():
 	pass
 	
 func physics_update(delta: float) -> void:
-	player.direction.x = Input.get_axis("move_left", "move_right")
-	
-	if not player.is_on_floor():
+	player.velocity.y += player.gravity * delta
+	if not Input.is_action_pressed("duck"):
+		state_machine.change_state(player.states.idle)
+	elif not player.is_on_floor():
 		state_machine.change_state(player.states.fall)
 	elif Input.is_action_just_pressed("jump"):
 		state_machine.change_state(player.states.jump)
-	elif Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
-		state_machine.change_state(player.states.run)
