6f8b7fc82dfa1dcbde5793de3a27b4f2c4d342a4
pozbliz <sebro@hushmail.com>
2025-08-16 20:42:20 +0900

    feat: add health, hitbox components; add enemy base

---
+++ entities/components/health_component.gd
@@ -0,0 +1,16 @@
+class_name HealthComponent
+extends Node
+
+
+@export var MAX_HEALTH: float = 1.0
+var health: float
+
+func _ready() -> void:
+	health = MAX_HEALTH
+
+
+func damage(attack: Attack):
+	health -= attack.attack_damage
+	
+	if health <= 0:
+		await get_parent().play_death_animation()
---
+++ entities/enemies/enemy.gd
@@ -0,0 +1,23 @@
+class_name Enemy
+extends CharacterBody2D
+
+
+signal enemy_died
+
+
+func _ready() -> void:
+	add_to_group("enemy")
+	if $AnimatedSprite2D:
+		$AnimatedSprite2D.play("default")
+
+func _process(delta: float) -> void:
+	pass
+
+func on_death():
+	if $AnimatedSprite2D:
+		$AnimatedSprite2D.play("death")
+		$HitboxComponent/CollisionShape2D.disabled = true
+		set_deferred("monitoring", false)
+		await $AnimatedSprite2D.animation_finished
+		EventBus.enemy.enemy_died.emit()
+	queue_free()
